dd8dcb1229c4123afc0bbe18046a3ead0c5730e3
keyboardmedicNL <125358148+keyboardmedicNL@users.noreply.github.com>
2023-11-19 02:16:00 +0100

    Update nocollisions.lua

--- nocollisions.lua
+++ nocollisions.lua
@@ -1,9 +1,9 @@
 local sim = ac.getSim()
 local car = ac.getCar(0)
 
-function script.update()
+function script.update(dt)
 collision = physics.disableCarCollisions(0);
 end
 
-script.update()
+script.update(dt)
 
